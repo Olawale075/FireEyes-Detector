af898d0f5c20dcbbf3e47d8bbd89ea98903832a0
Olawale075 <88412399+Olawale075@users.noreply.github.com>
2025-05-05 16:58:17 +0100

    sms added

---
+++ src/main/java/sms/com/sms/service/SmsService.java
@@ -0,0 +1,38 @@
+package sms.com.sms.service;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import sms.com.sms.dto.SmsRequest;
+
+@Service
+public class SmsService {
+
+    private final String TERMII_SMS_URL = "https://api.ng.termii.com/api/sms/send";
+
+    public String sendSms(String to, String message) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        SmsRequest smsRequest = new SmsRequest();
+        smsRequest.setTo(to);
+        smsRequest.setFrom("N-Alert"); // Must be approved by Termii
+        smsRequest.setSms(message);
+        smsRequest.setApi_key("TLbkDkdbzTvdwHKsyPAmTshoMKbFnqidbWRDpAxfpofaDiOGGBDpPCcwaHpuMk");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<SmsRequest> entity = new HttpEntity<>(smsRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                TERMII_SMS_URL,
+                HttpMethod.POST,
+                entity,
+                String.class
+        );
+
+        return response.getBody();
+    }
+}
+
